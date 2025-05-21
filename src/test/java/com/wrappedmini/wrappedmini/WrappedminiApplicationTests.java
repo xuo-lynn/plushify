400ad599ff913a200c5fec66bf9b94f9bce5c423
xuo <61861557+xuo-lynn@users.noreply.github.com>
2025-05-21 16:18:37 -0400

    springboot commit

---
+++ src/test/java/com/wrappedmini/wrappedmini/WrappedminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrappedmini.wrappedmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrappedminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
